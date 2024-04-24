56a23031c2fc05448d92a733591283de02addfba
Its-JoeTheKing <errfig.aymen.dev@gmail.com>
2024-04-24 23:27:26 +0100

    125

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 
 const chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
               'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+app.use(cors({
+	origin: "*"
+}));
 
 function randomize() {
 	var arr = [];
@@ -14,7 +19,8 @@ function randomize() {
 	return arr;
 }
 
-app.use(require('body-parser').urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 mongoose.connect("mongodb+srv://errfigaymendev:errfigaymendev@cluster0.t72lqss.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 const schema = new mongoose.Schema({
@@ -31,13 +37,15 @@ app.get("/search/:id", (req, res)=>{
 })
 
 app.post("/add", (req, res)=>{
+	var	i = randomize().join("");
+	var link = "https://vercel.shortly.com/"+i;
 	var data = new m({
-		id: randomize().join(""),
-		redirect: "https://vercel.shortly.com/"+randomize().join(""),
+		id: i,
+		redirect: link,
 		link: req.body.link
 	});
 	data.save();
-	res.send(req.body.link);
+	res.send(link);
 })
 
 app.listen(3000, ()=>{
